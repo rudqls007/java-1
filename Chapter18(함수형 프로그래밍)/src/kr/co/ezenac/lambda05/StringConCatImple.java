23a1b433435c6c4150b8311c8586c05ddb394f94
rudqls007 <rudqls007@gmail.com>
2022-11-19 14:17:15 +0900

    함수형 프로그래밍

---
+++ Chapter18(함수형 프로그래밍)/src/kr/co/ezenac/lambda05/StringConCatImple.java
@@ -0,0 +1,10 @@
+package kr.co.ezenac.lambda05;
+
+public class StringConCatImple implements StringConcat {
+
+	@Override
+	public void makeStirng(String s1, String s2) {
+		System.out.println(s1 + ", " + s2);
+	}
+
+}
