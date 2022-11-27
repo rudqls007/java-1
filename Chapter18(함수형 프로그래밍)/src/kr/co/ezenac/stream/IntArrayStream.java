c29b1d8bd76f539f72fa10ed2c794b8d5e48f3ac
rudqls007 <rudqls007@gmail.com>
2022-11-27 19:43:37 +0900

    array

---
+++ Chapter18(함수형 프로그래밍)/src/kr/co/ezenac/stream/IntArrayStream.java
@@ -0,0 +1,25 @@
+package kr.co.ezenac.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IntArrayStream {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		Arrays.stream(arr).forEach(n -> System.out.print(n + "\t"));
+		System.out.println();
+		
+		int sum = Arrays.stream(arr).sum();
+		System.out.println(" sum : " + sum);
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		int sum1 =list.stream().mapToInt(n -> n.intValue()).sum();
+		System.out.println(sum1);
+	}
+}
