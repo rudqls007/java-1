52354e4afe5c69b76d463ced28c6b3932d523dcf
rudqls007 <rudqls007@gmail.com>
2022-11-27 19:42:44 +0900

    Array

---
+++ Chapter18(함수형 프로그래밍)/src/kr/co/ezenac/stream/IntArrayTest.java
@@ -0,0 +1,17 @@
+package kr.co.ezenac.stream;
+
+import java.util.Arrays;
+
+public class IntArrayTest {
+
+	public static void main(String[] args) {
+		
+			int[] arr = {1, 2, 3, 4, 5};
+			int sum = Arrays.stream(arr).sum();
+			long count = Arrays.stream(arr).count();
+			
+			System.out.println(sum);
+			System.out.println(count);
+	}
+
+}
